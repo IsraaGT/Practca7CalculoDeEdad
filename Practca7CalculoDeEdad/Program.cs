4aea3a925e7372131aa89de318c822d175b86532
Isra <Isra@DESKTOP-64KKLPF>
2023-10-05 21:04:47 -0600

    Agregar archivos de proyecto.

---
+++ Practca7CalculoDeEdad/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("En qué año naciste? ");
+int AñoN = int.Parse(Console.ReadLine());
+
+int AñoA = DateTime.Now.Year;
+int edad = AñoA - AñoN;
+
+Console.WriteLine("Tu edad es " + edad + " años.");
+
+Console.ReadKey();
